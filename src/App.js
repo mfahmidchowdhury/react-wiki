41c67aaaef204bca1569be5ca6f83d836360a2c0
M. Fahmid Chowdhury <mfahmidchowdhury@gmail.com>
2023-04-23 17:50:56 -0600

    added fetch data functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+  let api = `https://rickandmortyapi.com/api/character${pageNumber}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">Wiki</span>
       </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
