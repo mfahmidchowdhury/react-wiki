3a1de23f1d01234097856e718f34d80f6cc741c1
M. Fahmid Chowdhury <mfahmidchowdhury@gmail.com>
2023-04-23 14:18:17 -0600

    margin

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/js/bootstrap";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center ubuntu">
+      <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">Wiki</span>
       </h1>
     </div>
